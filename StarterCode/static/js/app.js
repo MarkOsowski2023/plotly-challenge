e9bbb5b6dd666d372c99aa7bb9ab72aefec4d768
Mark <markosowski2023@u.northwestern.edu>
2022-09-28 18:49:36 -0500

    data finally loaded

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,8 @@
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+d3.json(url).then((data) => {
+    let metadata = data.metadata;
+    console.log(metadata);
+});
+
+
